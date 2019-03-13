be808d3313083835e9c9c9964e228e05e852f868
Alessandro Serpi <alessandro.serpi@me.com>
2019-03-13 15:44:04 +0100

    Add script to connect the host to a Kathara LAN

---
+++ netkit_connect.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+HELP=$(cat <<-EOM
+Usage: $0 [options]
+
+    -a| --host-address      address for the host interface [optional]
+    -h| --help              help [optional]
+    -i| --interface         interface base name
+    -n| --network           network as called in lab.conf
+    -o| --netkit-address    address for the netkit interface [optional]
+    -r| --route             add a route to the local interface [optional]
+EOM
+)
+
+PARAMS=""
+ROUTES=()
+
+while (( "$#" )); do
+  case "$1" in
+    -a|--host-address)
+      if [ ${ADDRESS_HOST+x} ]; then
+        echo "ERROR: Too many host addresses"
+        exit 2
+      fi
+      ADDRESS_HOST=$2
+      shift 2
+      ;;
+    -h|--help)
+      echo "$HELP"
+      exit 0
+      ;;
+    -i|--interface)
+      if [ ${INTERFACE+x} ]; then
+        echo "ERROR: Too many interfaces"
+        exit 2
+      fi
+      INTERFACE=$2
+      shift 2
+      ;;
+    -n|--network)
+      if [ ${NETWORK+x} ]; then
+        echo "ERROR: Too many networks"
+        exit 2
+      fi
+      NETWORK=$2
+      shift 2
+      ;;
+    -o|--netkit-address)
+      if [ ${ADDRESS_NETKIT+x} ]; then
+        echo "ERROR: Too many netkit addresses"
+        exit 2
+      fi
+      ADDRESS_NETKIT=$2
+      shift 2
+      ;;
+    -r|--route)
+      ROUTES+=($2)
+      shift 2
+      ;;
+    --) # end argument parsing
+      shift
+      break
+      ;;
+    -*|--*=) # unsupported flags
+      echo "Error: Unsupported parameter $1" >&2
+      exit 1
+      ;;
+    *) # preserve positional arguments
+      PARAMS="$PARAMS $1"
+      shift
+      ;;
+  esac
+done
+
+# set positional arguments in their proper place
+eval set -- "$PARAMS"
+
+
+if ! ( [ $INTERFACE ] && [ $NETWORK ] ); then
+  echo "$HELP"
+  exit 2
+fi
+
+if ! [[ `grep ${INTERFACE}_host /proc/net/dev` ]]; then
+  sudo ip link add dev ${INTERFACE}_netkit type veth peer name ${INTERFACE}_host
+  sudo ip link set ${INTERFACE}_host up
+  sudo ip link set ${INTERFACE}_netkit up
+fi
+
+if [[ $ADDRESS_HOST ]]; then
+  sudo ip addr flush dev ${INTERFACE}_host
+  sudo ip addr add $ADDRESS_HOST dev ${INTERFACE}_host
+fi
+
+if [[ $ADDRESS_NETKIT ]]; then
+  sudo ip addr flush dev ${INTERFACE}_netkit
+  sudo ip addr add $ADDRESS_NETKIT dev ${INTERFACE}_netkit
+fi
+
+DOCKER_NETWORK=$(sudo docker network ls | grep ".*netkit.*$NETWORK" | grep -Po "^(.*?)\s")
+DOCKER_NETWORK="br-${DOCKER_NETWORK%?}"
+
+sudo brctl addif $DOCKER_NETWORK ${INTERFACE}_netkit
+
+for route in "${ROUTES[@]}"; do
+  sudo ip route add $route dev ${INTERFACE}_host
+done
