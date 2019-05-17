d8f3cda70752887cc12983f9af46dd7de7e48ebc
Alessandro Serpi <alessandro.serpi@me.com>
2019-05-17 12:02:11 +0200

    Migrate configuration script to zsh

--- lab4/files/configure.sh
+++ lab4/files/configure.sh
@@ -1,19 +1,17 @@
-#!/bin/bash
+#!/bin/zsh
 
-[[ `sudo docker network ls | grep -E [[:space:]]netkit_0_DMZ` =~ ^[[:alnum:]]* ]] && echo -e "DMZ:\t\tbr-${BASH_REMATCH[0]}"
-
-if [[ `ip link show br-firewalls` = 'Device "br-firewalls" does not exist.' ]]; then
+[[ `docker network ls | grep -E "\snetkit_0_DMZ\s"` =~ "^\w*" ]] && echo -e "DMZ:\t\tbr-$MATCH"
+if ! [[ `ip link show br-firewalls 2> /dev/null` ]]; then
     brctl addbr br-firewalls
     ip link set br-firewalls up
 fi
 echo -e "Firewalls:\tbr-firewalls"
 
-[[ `sudo docker network ls | grep -E [[:space:]]netkit_0_server` =~ ^[[:alnum:]]* ]] && echo -e "Server:\t\tbr-${BASH_REMATCH[0]}"
+[[ `sudo docker network ls | grep -E "\snetkit_0_server\s"` =~ "^\w*" ]] && echo -e "Server:\t\tbr-$MATCH"
 
-[[ `sudo docker network ls | grep -E [[:space:]]netkit_0_client` =~ ^[[:alnum:]]* ]] && CLIENT="br-${BASH_REMATCH[0]}"
+[[ `sudo docker network ls | grep -E "\snetkit_0_client\s"` =~ "^\w*" ]] && CLIENT="br-$MATCH"
 ip route del 100.64.0.0/16 &> /dev/null
 ip address flush dev $CLIENT
 ip address add 100.64.2.5/24 dev $CLIENT
 ip route add 100.64.0.0/16 via 100.64.2.1
 echo -e "Client:\t\t$CLIENT"
-
