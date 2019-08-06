bd9efbd13b311acd09be28d029909cfe2486180b
Alessandro Serpi <alessandro.serpi@me.com>
2019-08-06 14:07:20 +0200

    Add domain controller to configuration script

--- lab6/files/start_vms.sh
+++ lab6/files/start_vms.sh
@@ -1,7 +1,18 @@
 #!/bin/zsh
 
+DC="pnd-dc"
 INTFW="pnd-intfw"
 MAINFW="pnd-mainfw"
+
+DC_START=false
+
+
+for arg in "$@"; do
+    if [[ $arg = "dc" ]]; then
+        DC_START=true
+    fi
+done
+
 
 [[ `sudo docker network ls | grep -E "\snetkit_0_DMZ\s"` =~ "^\w*" ]] && DMZ="br-$MATCH"
 VBoxManage modifyvm $MAINFW --bridgeadapter2 "$DMZ"
@@ -17,6 +28,7 @@ echo -e "Firewalls:\tbr-firewalls"
 
 [[ `sudo docker network ls | grep -E "\snetkit_0_server\s"` =~ "^\w*" ]] && SERVER="br-$MATCH"
 VBoxManage modifyvm $INTFW --bridgeadapter2 "$SERVER"
+VBoxManage modifyvm $DC --bridgeadapter1 "$SERVER"
 echo -e "Server:\t\t$SERVER"
 
 [[ `sudo docker network ls | grep -E "\snetkit_0_client\s"` =~ "^\w*" ]] && CLIENT="br-$MATCH"
@@ -29,3 +41,4 @@ echo -e "Client:\t\t$CLIENT"
 
 VBoxManage startvm "$INTFW"
 VBoxManage startvm "$MAINFW"
+if [ $DC_START ]; then VBoxManage startvm "$DC"; fi
